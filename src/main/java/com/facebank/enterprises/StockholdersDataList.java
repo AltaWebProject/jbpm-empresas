25202127838bcddbaf3c910ab2b5afca6656b544
wbadmin <>
2022-08-09 14:29:09 +0000

    {/src/main/java/com/facebank/enterprises/StockholdersDataList.java}

--- src/main/java/com/facebank/enterprises/StockholdersDataList.java
+++ src/main/java/com/facebank/enterprises/StockholdersDataList.java
@@ -6,12 +6,25 @@ package com.facebank.enterprises;
 
 public class StockholdersDataList implements java.io.Serializable {
 
-    static final long serialVersionUID = 1L;
+	static final long serialVersionUID = 1L;
 
-    public StockholdersDataList() {
-    }
+	private java.util.List<com.facebank.enterprises.StockholdersData> stockholdersData;
 
+	public StockholdersDataList() {
+	}
 
+	public java.util.List<com.facebank.enterprises.StockholdersData> getStockholdersData() {
+		return this.stockholdersData;
+	}
 
+	public void setStockholdersData(
+			java.util.List<com.facebank.enterprises.StockholdersData> stockholdersData) {
+		this.stockholdersData = stockholdersData;
+	}
+
+	public StockholdersDataList(
+			java.util.List<com.facebank.enterprises.StockholdersData> stockholdersData) {
+		this.stockholdersData = stockholdersData;
+	}
 
 }
