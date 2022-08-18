fd861f2db4f3739f0b92313d97a8c11b623fe7fc
wbadmin <>
2022-08-18 14:15:06 +0000

    {/src/main/java/com/facebank/enterprises/AuthorizedDataList.java}

--- src/main/java/com/facebank/enterprises/AuthorizedDataList.java
+++ src/main/java/com/facebank/enterprises/AuthorizedDataList.java
@@ -8,23 +8,10 @@ public class AuthorizedDataList implements java.io.Serializable {
 
 	static final long serialVersionUID = 1L;
 
-	@javax.persistence.GeneratedValue(generator = "AUTHORIZEDDATALIST_ID_GENERATOR", strategy = javax.persistence.GenerationType.AUTO)
-	@javax.persistence.Id
-	@javax.persistence.SequenceGenerator(sequenceName = "AUTHORIZEDDATALIST_ID_SEQ", name = "AUTHORIZEDDATALIST_ID_GENERATOR")
-	private java.lang.Long id;
-
 	@javax.persistence.OneToMany(fetch = javax.persistence.FetchType.EAGER, cascade = {javax.persistence.CascadeType.ALL})
 	private java.util.List<com.facebank.enterprises.AuthorizedData> authorizedData;
 
 	public AuthorizedDataList() {
-	}
-
-	public java.lang.Long getId() {
-		return this.id;
-	}
-
-	public void setId(java.lang.Long id) {
-		this.id = id;
 	}
 
 	public java.util.List<com.facebank.enterprises.AuthorizedData> getAuthorizedData() {
@@ -37,9 +24,7 @@ public class AuthorizedDataList implements java.io.Serializable {
 	}
 
 	public AuthorizedDataList(
-			java.lang.Long id,
 			java.util.List<com.facebank.enterprises.AuthorizedData> authorizedData) {
-		this.id = id;
 		this.authorizedData = authorizedData;
 	}
 
