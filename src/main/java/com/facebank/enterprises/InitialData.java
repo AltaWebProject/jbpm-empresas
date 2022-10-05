60f8555a6b34e52904261fc474740c4725f37d1a
wbadmin <>
2022-10-05 17:42:35 +0000

    {/src/main/java/com/facebank/enterprises/InitialData.java}

--- src/main/java/com/facebank/enterprises/InitialData.java
+++ src/main/java/com/facebank/enterprises/InitialData.java
@@ -12,9 +12,7 @@ public class InitialData implements java.io.Serializable {
 	private java.lang.String bdfEmail;
 	private java.lang.String bdfLoginName;
 	private java.lang.String bdfName;
-	private java.lang.String currentUser;
 	private java.lang.String email;
-	private java.lang.String completeNames;
 	@javax.persistence.Id
 	@javax.persistence.GeneratedValue(generator = "INITIALDATA_ID_GENERATOR", strategy = javax.persistence.GenerationType.AUTO)
 	@javax.persistence.SequenceGenerator(sequenceName = "INITIALDATA_ID_SEQ", name = "INITIALDATA_ID_GENERATOR")
@@ -22,15 +20,32 @@ public class InitialData implements java.io.Serializable {
 	private java.lang.String languageBDF;
 	private java.lang.String languageClient;
 	private java.lang.String login;
-	private java.lang.String products;
-	private java.lang.String reasonRequest;
-
 	@javax.persistence.ElementCollection(fetch = javax.persistence.FetchType.EAGER)
 	private java.util.List<java.lang.Integer> rolIds;
 
-	private java.lang.String firstSurname;
+	private java.lang.String surnameAdmin;
 
-	private java.lang.String secondSurname;
+	private java.lang.String nameAdmin;
+
+	private java.lang.String acronym;
+
+	private java.lang.String nationality;
+
+	private java.lang.String agency;
+
+	private java.lang.String executive;
+
+	private java.lang.String companyName;
+
+	private java.lang.String mainPhone;
+
+	private java.lang.String otherPhone;
+
+	private java.lang.String codeEntity;
+
+	private java.lang.String nameEntity;
+
+	private java.lang.String surNameEntity;
 
 	public InitialData() {
 	}
@@ -59,28 +74,12 @@ public class InitialData implements java.io.Serializable {
 		this.bdfName = bdfName;
 	}
 
-	public java.lang.String getCurrentUser() {
-		return this.currentUser;
-	}
-
-	public void setCurrentUser(java.lang.String currentUser) {
-		this.currentUser = currentUser;
-	}
-
 	public java.lang.String getEmail() {
 		return this.email;
 	}
 
 	public void setEmail(java.lang.String email) {
 		this.email = email;
-	}
-
-	public java.lang.String getCompleteNames() {
-		return this.completeNames;
-	}
-
-	public void setCompleteNames(java.lang.String completeNames) {
-		this.completeNames = completeNames;
 	}
 
 	public java.lang.Long getId() {
@@ -115,22 +114,6 @@ public class InitialData implements java.io.Serializable {
 		this.login = login;
 	}
 
-	public java.lang.String getProducts() {
-		return this.products;
-	}
-
-	public void setProducts(java.lang.String products) {
-		this.products = products;
-	}
-
-	public java.lang.String getReasonRequest() {
-		return this.reasonRequest;
-	}
-
-	public void setReasonRequest(java.lang.String reasonRequest) {
-		this.reasonRequest = reasonRequest;
-	}
-
 	public java.util.List<java.lang.Integer> getRolIds() {
 		return this.rolIds;
 	}
@@ -139,46 +122,134 @@ public class InitialData implements java.io.Serializable {
 		this.rolIds = rolIds;
 	}
 
-	public java.lang.String getFirstSurname() {
-		return this.firstSurname;
+	public java.lang.String getSurnameAdmin() {
+		return this.surnameAdmin;
 	}
 
-	public void setFirstSurname(java.lang.String firstSurname) {
-		this.firstSurname = firstSurname;
+	public void setSurnameAdmin(java.lang.String surnameAdmin) {
+		this.surnameAdmin = surnameAdmin;
 	}
 
-	public java.lang.String getSecondSurname() {
-		return this.secondSurname;
+	public java.lang.String getNameAdmin() {
+		return this.nameAdmin;
 	}
 
-	public void setSecondSurname(java.lang.String secondSurname) {
-		this.secondSurname = secondSurname;
+	public void setNameAdmin(java.lang.String nameAdmin) {
+		this.nameAdmin = nameAdmin;
+	}
+
+	public java.lang.String getAcronym() {
+		return this.acronym;
+	}
+
+	public void setAcronym(java.lang.String acronym) {
+		this.acronym = acronym;
+	}
+
+	public java.lang.String getNationality() {
+		return this.nationality;
+	}
+
+	public void setNationality(java.lang.String nationality) {
+		this.nationality = nationality;
+	}
+
+	public java.lang.String getAgency() {
+		return this.agency;
+	}
+
+	public void setAgency(java.lang.String agency) {
+		this.agency = agency;
+	}
+
+	public java.lang.String getExecutive() {
+		return this.executive;
+	}
+
+	public void setExecutive(java.lang.String executive) {
+		this.executive = executive;
+	}
+
+	public java.lang.String getCompanyName() {
+		return this.companyName;
+	}
+
+	public void setCompanyName(java.lang.String companyName) {
+		this.companyName = companyName;
+	}
+
+	public java.lang.String getMainPhone() {
+		return this.mainPhone;
+	}
+
+	public void setMainPhone(java.lang.String mainPhone) {
+		this.mainPhone = mainPhone;
+	}
+
+	public java.lang.String getOtherPhone() {
+		return this.otherPhone;
+	}
+
+	public void setOtherPhone(java.lang.String otherPhone) {
+		this.otherPhone = otherPhone;
+	}
+
+	public java.lang.String getCodeEntity() {
+		return this.codeEntity;
+	}
+
+	public void setCodeEntity(java.lang.String codeEntity) {
+		this.codeEntity = codeEntity;
+	}
+
+	public java.lang.String getNameEntity() {
+		return this.nameEntity;
+	}
+
+	public void setNameEntity(java.lang.String nameEntity) {
+		this.nameEntity = nameEntity;
+	}
+
+	public java.lang.String getSurNameEntity() {
+		return this.surNameEntity;
+	}
+
+	public void setSurNameEntity(java.lang.String surNameEntity) {
+		this.surNameEntity = surNameEntity;
 	}
 
 	public InitialData(java.lang.String bdfEmail,
 			java.lang.String bdfLoginName, java.lang.String bdfName,
-			java.lang.String currentUser, java.lang.String email,
-			java.lang.String completeNames, java.lang.Long id,
+			java.lang.String email, java.lang.Long id,
 			java.lang.String languageBDF, java.lang.String languageClient,
-			java.lang.String login, java.lang.String products,
-			java.lang.String reasonRequest,
-			java.util.List<java.lang.Integer> rolIds,
-			java.lang.String firstSurname, java.lang.String secondSurname) {
+			java.lang.String login, java.util.List<java.lang.Integer> rolIds,
+			java.lang.String surnameAdmin, java.lang.String nameAdmin,
+			java.lang.String acronym, java.lang.String nationality,
+			java.lang.String agency, java.lang.String executive,
+			java.lang.String companyName, java.lang.String mainPhone,
+			java.lang.String otherPhone, java.lang.String codeEntity,
+			java.lang.String nameEntity, java.lang.String surNameEntity) {
 		this.bdfEmail = bdfEmail;
 		this.bdfLoginName = bdfLoginName;
 		this.bdfName = bdfName;
-		this.currentUser = currentUser;
 		this.email = email;
-		this.completeNames = completeNames;
 		this.id = id;
 		this.languageBDF = languageBDF;
 		this.languageClient = languageClient;
 		this.login = login;
-		this.products = products;
-		this.reasonRequest = reasonRequest;
 		this.rolIds = rolIds;
-		this.firstSurname = firstSurname;
-		this.secondSurname = secondSurname;
+		this.surnameAdmin = surnameAdmin;
+		this.nameAdmin = nameAdmin;
+		this.acronym = acronym;
+		this.nationality = nationality;
+		this.agency = agency;
+		this.executive = executive;
+		this.companyName = companyName;
+		this.mainPhone = mainPhone;
+		this.otherPhone = otherPhone;
+		this.codeEntity = codeEntity;
+		this.nameEntity = nameEntity;
+		this.surNameEntity = surNameEntity;
 	}
 
 }
