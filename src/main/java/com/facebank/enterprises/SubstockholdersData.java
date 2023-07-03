cbdf4fd2cad72d5403aaff2495fe35a75c2e4c99
wbadmin <>
2023-07-03 18:00:05 +0000

    {/src/main/java/com/facebank/enterprises/SubstockholdersData.java}

--- src/main/java/com/facebank/enterprises/SubstockholdersData.java
+++ src/main/java/com/facebank/enterprises/SubstockholdersData.java
@@ -12,27 +12,35 @@ public class SubstockholdersData implements java.io.Serializable {
 
 	private java.lang.String idStockholderRelated;
 
-	private java.lang.String nameBeneficiary;
-
-	private java.lang.String nationalityBeneficiary;
-
-	private java.lang.Double participationBeneficiary;
-
-	private java.lang.String positionBeneficiary;
-
-	private java.lang.String secondSurNameBeneficiary;
-
-	private java.lang.String surNameBeneficiary;
-
-	private java.lang.String typeBeneficiary;
-
-	private java.lang.String idBeneficiary;
-
-	private java.lang.String secondNameBeneficiary;
-
-	private java.lang.String nationalityValue;
-
 	private java.lang.String companyName;
+
+	private java.lang.String nameSubstockholder;
+
+	private java.lang.Double participationSubstockholder;
+
+	private java.lang.String secondSurNameSubstockholder;
+
+	private java.lang.String surNameSubstockholder;
+
+	private java.lang.String typeSubstockholder;
+
+	private java.lang.Integer idNumberSubstockholder;
+
+	private java.lang.String secondNameSubstockholder;
+
+	private java.lang.String birthDateSubstockholder;
+
+	private java.lang.String emailSubstockholder;
+
+	private java.lang.String addressSubstockholder;
+
+	private java.lang.String citySubstockholder;
+
+	private java.lang.String stateSubstockholder;
+
+	private java.lang.String countrySubstockholder;
+
+	private java.lang.String issuingCountrySubstockholder;
 
 	public SubstockholdersData() {
 	}
@@ -53,89 +61,6 @@ public class SubstockholdersData implements java.io.Serializable {
 		this.idStockholderRelated = idStockholderRelated;
 	}
 
-	public java.lang.String getNameBeneficiary() {
-		return this.nameBeneficiary;
-	}
-
-	public void setNameBeneficiary(java.lang.String nameBeneficiary) {
-		this.nameBeneficiary = nameBeneficiary;
-	}
-
-	public java.lang.String getNationalityBeneficiary() {
-		return this.nationalityBeneficiary;
-	}
-
-	public void setNationalityBeneficiary(
-			java.lang.String nationalityBeneficiary) {
-		this.nationalityBeneficiary = nationalityBeneficiary;
-	}
-
-	public java.lang.Double getParticipationBeneficiary() {
-		return this.participationBeneficiary;
-	}
-
-	public void setParticipationBeneficiary(
-			java.lang.Double participationBeneficiary) {
-		this.participationBeneficiary = participationBeneficiary;
-	}
-
-	public java.lang.String getPositionBeneficiary() {
-		return this.positionBeneficiary;
-	}
-
-	public void setPositionBeneficiary(java.lang.String positionBeneficiary) {
-		this.positionBeneficiary = positionBeneficiary;
-	}
-
-	public java.lang.String getSecondSurNameBeneficiary() {
-		return this.secondSurNameBeneficiary;
-	}
-
-	public void setSecondSurNameBeneficiary(
-			java.lang.String secondSurNameBeneficiary) {
-		this.secondSurNameBeneficiary = secondSurNameBeneficiary;
-	}
-
-	public java.lang.String getSurNameBeneficiary() {
-		return this.surNameBeneficiary;
-	}
-
-	public void setSurNameBeneficiary(java.lang.String surNameBeneficiary) {
-		this.surNameBeneficiary = surNameBeneficiary;
-	}
-
-	public java.lang.String getTypeBeneficiary() {
-		return this.typeBeneficiary;
-	}
-
-	public void setTypeBeneficiary(java.lang.String typeBeneficiary) {
-		this.typeBeneficiary = typeBeneficiary;
-	}
-
-	public java.lang.String getIdBeneficiary() {
-		return this.idBeneficiary;
-	}
-
-	public void setIdBeneficiary(java.lang.String idBeneficiary) {
-		this.idBeneficiary = idBeneficiary;
-	}
-
-	public java.lang.String getSecondNameBeneficiary() {
-		return this.secondNameBeneficiary;
-	}
-
-	public void setSecondNameBeneficiary(java.lang.String secondNameBeneficiary) {
-		this.secondNameBeneficiary = secondNameBeneficiary;
-	}
-
-	public java.lang.String getNationalityValue() {
-		return this.nationalityValue;
-	}
-
-	public void setNationalityValue(java.lang.String nationalityValue) {
-		this.nationalityValue = nationalityValue;
-	}
-
 	public java.lang.String getCompanyName() {
 		return this.companyName;
 	}
@@ -144,30 +69,157 @@ public class SubstockholdersData implements java.io.Serializable {
 		this.companyName = companyName;
 	}
 
+	public java.lang.String getNameSubstockholder() {
+		return this.nameSubstockholder;
+	}
+
+	public void setNameSubstockholder(java.lang.String nameSubstockholder) {
+		this.nameSubstockholder = nameSubstockholder;
+	}
+
+	public java.lang.Double getParticipationSubstockholder() {
+		return this.participationSubstockholder;
+	}
+
+	public void setParticipationSubstockholder(
+			java.lang.Double participationSubstockholder) {
+		this.participationSubstockholder = participationSubstockholder;
+	}
+
+	public java.lang.String getSecondSurNameSubstockholder() {
+		return this.secondSurNameSubstockholder;
+	}
+
+	public void setSecondSurNameSubstockholder(
+			java.lang.String secondSurNameSubstockholder) {
+		this.secondSurNameSubstockholder = secondSurNameSubstockholder;
+	}
+
+	public java.lang.String getSurNameSubstockholder() {
+		return this.surNameSubstockholder;
+	}
+
+	public void setSurNameSubstockholder(java.lang.String surNameSubstockholder) {
+		this.surNameSubstockholder = surNameSubstockholder;
+	}
+
+	public java.lang.String getTypeSubstockholder() {
+		return this.typeSubstockholder;
+	}
+
+	public void setTypeSubstockholder(java.lang.String typeSubstockholder) {
+		this.typeSubstockholder = typeSubstockholder;
+	}
+
+	public java.lang.Integer getIdNumberSubstockholder() {
+		return this.idNumberSubstockholder;
+	}
+
+	public void setIdNumberSubstockholder(
+			java.lang.Integer idNumberSubstockholder) {
+		this.idNumberSubstockholder = idNumberSubstockholder;
+	}
+
+	public java.lang.String getSecondNameSubstockholder() {
+		return this.secondNameSubstockholder;
+	}
+
+	public void setSecondNameSubstockholder(
+			java.lang.String secondNameSubstockholder) {
+		this.secondNameSubstockholder = secondNameSubstockholder;
+	}
+
+	public java.lang.String getBirthDateSubstockholder() {
+		return this.birthDateSubstockholder;
+	}
+
+	public void setBirthDateSubstockholder(
+			java.lang.String birthDateSubstockholder) {
+		this.birthDateSubstockholder = birthDateSubstockholder;
+	}
+
+	public java.lang.String getEmailSubstockholder() {
+		return this.emailSubstockholder;
+	}
+
+	public void setEmailSubstockholder(java.lang.String emailSubstockholder) {
+		this.emailSubstockholder = emailSubstockholder;
+	}
+
+	public java.lang.String getAddressSubstockholder() {
+		return this.addressSubstockholder;
+	}
+
+	public void setAddressSubstockholder(java.lang.String addressSubstockholder) {
+		this.addressSubstockholder = addressSubstockholder;
+	}
+
+	public java.lang.String getCitySubstockholder() {
+		return this.citySubstockholder;
+	}
+
+	public void setCitySubstockholder(java.lang.String citySubstockholder) {
+		this.citySubstockholder = citySubstockholder;
+	}
+
+	public java.lang.String getStateSubstockholder() {
+		return this.stateSubstockholder;
+	}
+
+	public void setStateSubstockholder(java.lang.String stateSubstockholder) {
+		this.stateSubstockholder = stateSubstockholder;
+	}
+
+	public java.lang.String getCountrySubstockholder() {
+		return this.countrySubstockholder;
+	}
+
+	public void setCountrySubstockholder(java.lang.String countrySubstockholder) {
+		this.countrySubstockholder = countrySubstockholder;
+	}
+
+	public java.lang.String getIssuingCountrySubstockholder() {
+		return this.issuingCountrySubstockholder;
+	}
+
+	public void setIssuingCountrySubstockholder(
+			java.lang.String issuingCountrySubstockholder) {
+		this.issuingCountrySubstockholder = issuingCountrySubstockholder;
+	}
+
 	public SubstockholdersData(java.lang.String signatory,
 			java.lang.String idStockholderRelated,
-			java.lang.String nameBeneficiary,
-			java.lang.String nationalityBeneficiary,
-			java.lang.Double participationBeneficiary,
-			java.lang.String positionBeneficiary,
-			java.lang.String secondSurNameBeneficiary,
-			java.lang.String surNameBeneficiary,
-			java.lang.String typeBeneficiary, java.lang.String idBeneficiary,
-			java.lang.String secondNameBeneficiary,
-			java.lang.String nationalityValue, java.lang.String companyName) {
+			java.lang.String companyName, java.lang.String nameSubstockholder,
+			java.lang.Double participationSubstockholder,
+			java.lang.String secondSurNameSubstockholder,
+			java.lang.String surNameSubstockholder,
+			java.lang.String typeSubstockholder,
+			java.lang.Integer idNumberSubstockholder,
+			java.lang.String secondNameSubstockholder,
+			java.lang.String birthDateSubstockholder,
+			java.lang.String emailSubstockholder,
+			java.lang.String addressSubstockholder,
+			java.lang.String citySubstockholder,
+			java.lang.String stateSubstockholder,
+			java.lang.String countrySubstockholder,
+			java.lang.String issuingCountrySubstockholder) {
 		this.signatory = signatory;
 		this.idStockholderRelated = idStockholderRelated;
-		this.nameBeneficiary = nameBeneficiary;
-		this.nationalityBeneficiary = nationalityBeneficiary;
-		this.participationBeneficiary = participationBeneficiary;
-		this.positionBeneficiary = positionBeneficiary;
-		this.secondSurNameBeneficiary = secondSurNameBeneficiary;
-		this.surNameBeneficiary = surNameBeneficiary;
-		this.typeBeneficiary = typeBeneficiary;
-		this.idBeneficiary = idBeneficiary;
-		this.secondNameBeneficiary = secondNameBeneficiary;
-		this.nationalityValue = nationalityValue;
 		this.companyName = companyName;
+		this.nameSubstockholder = nameSubstockholder;
+		this.participationSubstockholder = participationSubstockholder;
+		this.secondSurNameSubstockholder = secondSurNameSubstockholder;
+		this.surNameSubstockholder = surNameSubstockholder;
+		this.typeSubstockholder = typeSubstockholder;
+		this.idNumberSubstockholder = idNumberSubstockholder;
+		this.secondNameSubstockholder = secondNameSubstockholder;
+		this.birthDateSubstockholder = birthDateSubstockholder;
+		this.emailSubstockholder = emailSubstockholder;
+		this.addressSubstockholder = addressSubstockholder;
+		this.citySubstockholder = citySubstockholder;
+		this.stateSubstockholder = stateSubstockholder;
+		this.countrySubstockholder = countrySubstockholder;
+		this.issuingCountrySubstockholder = issuingCountrySubstockholder;
 	}
 
 }
