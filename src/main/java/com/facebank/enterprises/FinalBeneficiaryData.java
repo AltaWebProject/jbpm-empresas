42bc26b445553e04a7ac630b539819f9d8299ed7
wbadmin <>
2023-07-03 14:56:08 +0000

    {/src/main/java/com/facebank/enterprises/FinalBeneficiaryData.java}

--- src/main/java/com/facebank/enterprises/FinalBeneficiaryData.java
+++ src/main/java/com/facebank/enterprises/FinalBeneficiaryData.java
@@ -6,12 +6,241 @@ package com.facebank.enterprises;
 
 public class FinalBeneficiaryData implements java.io.Serializable {
 
-    static final long serialVersionUID = 1L;
+	static final long serialVersionUID = 1L;
 
-    public FinalBeneficiaryData() {
-    }
+	private java.lang.String signatory;
 
+	private java.lang.String idStockholderRelated;
 
+	private java.lang.String nameBeneficiary;
 
+	private java.lang.String nationalityBeneficiary;
+
+	private java.lang.Double participationBeneficiary;
+
+	private java.lang.String secondSurNameBeneficiary;
+
+	private java.lang.String surNameBeneficiary;
+
+	private java.lang.String typeBeneficiary;
+
+	private java.lang.String idBeneficiary;
+
+	private java.lang.String secondNameBeneficiary;
+
+	private java.lang.String nationalityValue;
+
+	private java.lang.String companyName;
+
+	private java.lang.String birthDateBeneficiary;
+
+	private java.lang.String emailBeneficiary;
+
+	private java.lang.String addressBeneficiary;
+
+	private java.lang.String cityBeneficiary;
+
+	private java.lang.String stateBeneficiary;
+
+	private java.lang.String countryBeneficiary;
+
+	private java.lang.String issuingCountryBeneficiary;
+
+	public FinalBeneficiaryData() {
+	}
+
+	public java.lang.String getSignatory() {
+		return this.signatory;
+	}
+
+	public void setSignatory(java.lang.String signatory) {
+		this.signatory = signatory;
+	}
+
+	public java.lang.String getIdStockholderRelated() {
+		return this.idStockholderRelated;
+	}
+
+	public void setIdStockholderRelated(java.lang.String idStockholderRelated) {
+		this.idStockholderRelated = idStockholderRelated;
+	}
+
+	public java.lang.String getNameBeneficiary() {
+		return this.nameBeneficiary;
+	}
+
+	public void setNameBeneficiary(java.lang.String nameBeneficiary) {
+		this.nameBeneficiary = nameBeneficiary;
+	}
+
+	public java.lang.String getNationalityBeneficiary() {
+		return this.nationalityBeneficiary;
+	}
+
+	public void setNationalityBeneficiary(
+			java.lang.String nationalityBeneficiary) {
+		this.nationalityBeneficiary = nationalityBeneficiary;
+	}
+
+	public java.lang.Double getParticipationBeneficiary() {
+		return this.participationBeneficiary;
+	}
+
+	public void setParticipationBeneficiary(
+			java.lang.Double participationBeneficiary) {
+		this.participationBeneficiary = participationBeneficiary;
+	}
+
+	public java.lang.String getSecondSurNameBeneficiary() {
+		return this.secondSurNameBeneficiary;
+	}
+
+	public void setSecondSurNameBeneficiary(
+			java.lang.String secondSurNameBeneficiary) {
+		this.secondSurNameBeneficiary = secondSurNameBeneficiary;
+	}
+
+	public java.lang.String getSurNameBeneficiary() {
+		return this.surNameBeneficiary;
+	}
+
+	public void setSurNameBeneficiary(java.lang.String surNameBeneficiary) {
+		this.surNameBeneficiary = surNameBeneficiary;
+	}
+
+	public java.lang.String getTypeBeneficiary() {
+		return this.typeBeneficiary;
+	}
+
+	public void setTypeBeneficiary(java.lang.String typeBeneficiary) {
+		this.typeBeneficiary = typeBeneficiary;
+	}
+
+	public java.lang.String getIdBeneficiary() {
+		return this.idBeneficiary;
+	}
+
+	public void setIdBeneficiary(java.lang.String idBeneficiary) {
+		this.idBeneficiary = idBeneficiary;
+	}
+
+	public java.lang.String getSecondNameBeneficiary() {
+		return this.secondNameBeneficiary;
+	}
+
+	public void setSecondNameBeneficiary(java.lang.String secondNameBeneficiary) {
+		this.secondNameBeneficiary = secondNameBeneficiary;
+	}
+
+	public java.lang.String getNationalityValue() {
+		return this.nationalityValue;
+	}
+
+	public void setNationalityValue(java.lang.String nationalityValue) {
+		this.nationalityValue = nationalityValue;
+	}
+
+	public java.lang.String getCompanyName() {
+		return this.companyName;
+	}
+
+	public void setCompanyName(java.lang.String companyName) {
+		this.companyName = companyName;
+	}
+
+	public java.lang.String getBirthDateBeneficiary() {
+		return this.birthDateBeneficiary;
+	}
+
+	public void setBirthDateBeneficiary(java.lang.String birthDateBeneficiary) {
+		this.birthDateBeneficiary = birthDateBeneficiary;
+	}
+
+	public java.lang.String getEmailBeneficiary() {
+		return this.emailBeneficiary;
+	}
+
+	public void setEmailBeneficiary(java.lang.String emailBeneficiary) {
+		this.emailBeneficiary = emailBeneficiary;
+	}
+
+	public java.lang.String getAddressBeneficiary() {
+		return this.addressBeneficiary;
+	}
+
+	public void setAddressBeneficiary(java.lang.String addressBeneficiary) {
+		this.addressBeneficiary = addressBeneficiary;
+	}
+
+	public java.lang.String getCityBeneficiary() {
+		return this.cityBeneficiary;
+	}
+
+	public void setCityBeneficiary(java.lang.String cityBeneficiary) {
+		this.cityBeneficiary = cityBeneficiary;
+	}
+
+	public java.lang.String getStateBeneficiary() {
+		return this.stateBeneficiary;
+	}
+
+	public void setStateBeneficiary(java.lang.String stateBeneficiary) {
+		this.stateBeneficiary = stateBeneficiary;
+	}
+
+	public java.lang.String getCountryBeneficiary() {
+		return this.countryBeneficiary;
+	}
+
+	public void setCountryBeneficiary(java.lang.String countryBeneficiary) {
+		this.countryBeneficiary = countryBeneficiary;
+	}
+
+	public java.lang.String getIssuingCountryBeneficiary() {
+		return this.issuingCountryBeneficiary;
+	}
+
+	public void setIssuingCountryBeneficiary(
+			java.lang.String issuingCountryBeneficiary) {
+		this.issuingCountryBeneficiary = issuingCountryBeneficiary;
+	}
+
+	public FinalBeneficiaryData(java.lang.String signatory,
+			java.lang.String idStockholderRelated,
+			java.lang.String nameBeneficiary,
+			java.lang.String nationalityBeneficiary,
+			java.lang.Double participationBeneficiary,
+			java.lang.String secondSurNameBeneficiary,
+			java.lang.String surNameBeneficiary,
+			java.lang.String typeBeneficiary, java.lang.String idBeneficiary,
+			java.lang.String secondNameBeneficiary,
+			java.lang.String nationalityValue, java.lang.String companyName,
+			java.lang.String birthDateBeneficiary,
+			java.lang.String emailBeneficiary,
+			java.lang.String addressBeneficiary,
+			java.lang.String cityBeneficiary,
+			java.lang.String stateBeneficiary,
+			java.lang.String countryBeneficiary,
+			java.lang.String issuingCountryBeneficiary) {
+		this.signatory = signatory;
+		this.idStockholderRelated = idStockholderRelated;
+		this.nameBeneficiary = nameBeneficiary;
+		this.nationalityBeneficiary = nationalityBeneficiary;
+		this.participationBeneficiary = participationBeneficiary;
+		this.secondSurNameBeneficiary = secondSurNameBeneficiary;
+		this.surNameBeneficiary = surNameBeneficiary;
+		this.typeBeneficiary = typeBeneficiary;
+		this.idBeneficiary = idBeneficiary;
+		this.secondNameBeneficiary = secondNameBeneficiary;
+		this.nationalityValue = nationalityValue;
+		this.companyName = companyName;
+		this.birthDateBeneficiary = birthDateBeneficiary;
+		this.emailBeneficiary = emailBeneficiary;
+		this.addressBeneficiary = addressBeneficiary;
+		this.cityBeneficiary = cityBeneficiary;
+		this.stateBeneficiary = stateBeneficiary;
+		this.countryBeneficiary = countryBeneficiary;
+		this.issuingCountryBeneficiary = issuingCountryBeneficiary;
+	}
 
 }
